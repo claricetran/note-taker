56de6c485caa9f6f4dd244034201f48addbe4534
Clarice Tran <tran.clarice@gmail.com>
2023-01-31 06:20:49 -0800

    Created server.js to handle paths for site. Created router folder with index.js to handle /api/notes get, post, and delete requests. Created fsUtil.js to handle fs and util modules. Created middleware clog.js to help know which endpoint is requested.

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const { clog } = require("./middleware/clog.js");
+const api = require("./routes/index.js");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// middleware clog
+app.use(clog);
+
+//middleware to parse JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
+
+app.use(express.static("public"));
+
+// GET route for notes page
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
+
+// GET route where wildcard will redirect to homepage
+app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
